eb8d76680d487d1db62fb0ca95d2e087db6fe341
user <patalah.e@mail.ru>
2025-11-05 13:33:27 +0300

    add tests and ci/cd workflows

---
+++ tests/test_user.py
@@ -0,0 +1,72 @@
+from fastapi.testclient import TestClient
+from src.main import app 
+
+client = TestClient(app)
+
+# Существующие пользователи
+users = [
+    {
+        'id': 1,
+        'name': 'Ivan Ivanov',
+        'email': 'i.i.ivanov@mail.com',
+    },
+    {
+        'id': 2,
+        'name': 'Petr Petrov',
+        'email': 'p.p.petrov@mail.com',
+    }
+]
+
+def test_get_existed_user():
+    '''Получение существующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 200
+    assert response.json() == users[0]
+
+def test_get_unexisted_user():
+    '''Получение несуществующего пользователя'''
+    # При отсутствии пользователя возвращается 404
+    response = client.get("/api/v1/user", params={'email': 'unexisted@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+def test_create_user_with_valid_email():
+    '''Создание пользователя с уникальной почтой'''
+    new_user_data = {
+        'name': 'Sidor Sidorov',
+        'email': 's.s.sidorov@mail.com'
+    }
+    # При успехе возвращается 201 и id
+    response = client.post("/api/v1/user", json=new_user_data)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int) # Проверка, что вернулся id (int)
+    
+    # Дополнительная проверка по коду 200
+    get_response = client.get("/api/v1/user", params={'email': new_user_data['email']})
+    assert get_response.status_code == 200
+    assert get_response.json()['name'] == new_user_data['name']
+
+def test_create_user_with_invalid_email():
+    '''Создание пользователя с почтой, которую использует другой пользователь'''
+    # Использование email существующего пользователя
+    invalid_data = {
+        'name': 'Duplicate User',
+        'email': users[0]['email'] 
+    }
+    # При дубликате почты возвращается 409
+    response = client.post("/api/v1/user", json=invalid_data)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
+
+def test_delete_user():
+    '''Удаление пользователя'''
+    # Использование email второго пользователя
+    email_to_delete = users[1]['email']
+    
+    # При удалении возвращается 204
+    response = client.delete("/api/v1/user", params={'email': email_to_delete})
+    assert response.status_code == 204
+    
+    # Дополнительная проверка по 404
+    get_response = client.get("/api/v1/user", params={'email': email_to_delete})
+    assert get_response.status_code == 404
